c4b943c4f568912a12d91c54cbe2ee17bb41bc82
araya-y <arayayemanegebremichael23@gmail.com>
2023-02-27 18:41:30 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Swap_int - swap the value of two integers
+ * @a: int a
+ * @b: int b
+ */
+void swap_int(int xvm*a, int *b)
+{
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
