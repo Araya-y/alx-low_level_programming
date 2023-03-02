68d3a7b11cff69d440a473f98e1a94360f204053
araya-y <arayayemanegebremichael23@gmail.com>
2023-03-01 18:40:58 -0800

    Done with all mandatory task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string with n
+ ** _strncpy - copies a string with n
  * @dest: copy to
  * @src: copy from
- * @n: number of char to be printed
+ * @n: number of char to be copied
  * Return: dest
  */
-char *_strncpy(char *dest, char src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
