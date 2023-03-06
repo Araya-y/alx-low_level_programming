34faf7a39f46a3b8be54a831952a15b98ba84d83
araya-y <arayayemanegebremichael23@gmail.com>
2023-03-05 17:36:06 -0800

    task 0

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,9 +1,8 @@
-#include "main.h"
 #include <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c - the character to print
- *
  * return on success 1
  * on error: -1 is returned, and errno is set appropriately 
  */
