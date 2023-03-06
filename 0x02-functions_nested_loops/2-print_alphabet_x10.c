dfb94a1bcf86f61ab1d68ce1c98dbc51471f3568
araya-y <arayayemanegebremichael23@gmail.com>
2023-03-05 17:53:48 -0800

    task 1, 2, 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * print _alphabet_x10 - prints 10 times the alphabet in lowercase
- * return: void
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * Return: always 0
  */
+
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	char ca;
+	int i;
 
+	i = 0;
 	while (i < 10)
 	{
 		c = 'a';
