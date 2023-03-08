eee3b373e20f2bd70c1dc00590640957763418c7
araya-y <arayayemanegebremichael23@gmail.com>
2023-03-07 19:53:40 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to be counted it's length
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length = length + _strlen_recursion(s + 1);
+	}
+	return (length);
+}
